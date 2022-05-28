365a9d02a92a382da779663d8a14a4100809861a
Anwesh Mishra <anweshm3@gmail.com>
2022-05-28 16:31:24 +0530

    Initial Commit

---
+++ assets/request.js
@@ -0,0 +1,25 @@
+const key = 'e5e73ae9a6cd67b5a7258cb9819364d2'
+
+// const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=e5e73ae9a6cd67b5a7258cb9819364d2'
+
+// fetch(baseURL)
+//    .then((data) => {
+//       console.log('Response', data.json)
+//    })
+//    .catch((error) => {
+//       console.log('Error')
+//    })
+
+const requestCity = async (city) => {
+   const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+   const query = `?q=${city}&appid=${key}`
+
+   // make fetch call (promise call)
+   const response = await fetch(baseURL + query)
+
+   // promise data
+   const data = await response.json();
+   // console.log(data)
+   return data
+}
+// requestCity('Mumbai')
